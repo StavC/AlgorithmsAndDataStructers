804a8d03100b85f58bd22f30cb0223fd73c0f2a4
Stav Cohen <cohenstav1@gmail.com>
2019-12-04 19:36:48 +0200

    leetcode

---
+++ Trees/InOrderIterative.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
